831c6f60c1ecb05137370faeba11dd60f883f42c
Richard Montgomery <rikermonty@gmail.com>
2017-12-05 10:20:54 -0800

    completed some Comment Responses/Reviewed Code

--- richard-mitchell/scripts/article.js
+++ richard-mitchell/scripts/article.js
@@ -9,11 +9,11 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-// REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
+// REVIEWED: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
 // COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// It contains the contextual this... therefore, fat arrow syntax is illegal.
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
@@ -21,7 +21,7 @@ Article.prototype.toHtml = function() {
 
   // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // It's an if statement written as a ternary condition. The question mark represents if the condition is true. The colon represents else.
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -33,7 +33,7 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// We call the function in Article.fetchAll. rawData is a parameter now, or a placeholder. Previously rawData was our object literals that we compiled through Handlebars from a separate .js file into our template to append to the HTML.
 Article.loadAll = rawData => {
   rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
@@ -48,6 +48,17 @@ Article.fetchAll = () => {
     Article.loadAll();
 
   } else {
+
+    $.ajax ({
+      url: './data/hackerlpsum.json',
+      method: 'GET',
+      success: function (data) {
+        console.log('data', data);
+      },
+      fail: function (err) {
+        console.log('Err', err);
+      }
+    })
 
   }
 }
